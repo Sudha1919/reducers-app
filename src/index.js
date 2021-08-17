41f7dac46db500d6cbf65be09853f8047e10c2f3
Sudha1919 <sudhabangaru1919@gmail.com>
2021-08-17 20:48:44 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const reduxStore = createStore(reducermain);
+
+ReactDOM.render(<Provider store={}>
+  <App />
+  </Provider>, document.getElementById("root"));
