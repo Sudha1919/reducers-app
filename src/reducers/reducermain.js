93b6885dabca157c3cdd60fa366a33795a48755a
Sudha1919 <sudhabangaru1919@gmail.com>
2021-08-17 20:45:40 +0530

    1 file modified, 1 added

---
+++ src/reducers/reducermain.js
@@ -0,0 +1,12 @@
+function reducermain(
+  state = {
+    Name: 'Sudha Reddy',
+    Employees: [
+      { Name: 'Sudha', Age: 28, Designation: 'UI Developer' },
+      { Name: 'Sandeep', Age: 32, Designation: 'DBA' }
+    ]
+  }
+) {
+  return state;
+}
+export default reducermain;
